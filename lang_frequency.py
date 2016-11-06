64ab60d1ed4a2fc79e0b533df7f4c51e9b1ab571
aspekt112 <aspekt112@gmail.com>
2016-11-06 18:25:40 +0300

    Задание №5 выполнено!

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import os
+import re
+from collections import OrderedDict
 
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r') as file_handler:
+        return file_handler.read()
 
 
 def get_most_frequent_words(text):
-    pass
+    splited_text = text.split()
+    words = set(splited_text)
+    counts = {}
+    for word in words:
+        counts[word] = splited_text.count(word)
+    ordered_counts = OrderedDict(sorted(counts.items(),
+                                        key=lambda t: t[1], reverse=True))
+    for count_number, count in enumerate(ordered_counts):
+        print('{0} - {1}'.format(count, ordered_counts[count]))
+        if count_number == 9:
+            break
+
+
+def clear_text(raw_text):
+    return re.sub('–|\.|,|…|\[|\]|!|\?|\(|\)|«|»|;|:|[0-9]|', '', raw_text)
 
 
 if __name__ == '__main__':
-    pass
+    path = str(input('Введите путь к файлу: '))
+    text = load_data(path)
+    if text is None:
+        print('Файл не найден. См. пример использования.')
+    else:
+        cleared_text = clear_text(text)
+        get_most_frequent_words(cleared_text)
